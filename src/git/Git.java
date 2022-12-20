8991bcd3d7d7f6912c9c6c219414d667be2b1e30
unknown <ashiqar7@gmail.com>
2022-12-20 07:27:40 -0800

    leityeur

---
+++ src/git/Git.java
@@ -0,0 +1,23 @@
+package git;
+
+public class Git {
+	
+	public void m1() {
+
+		System.out.println("Aq");
+	}
+	
+	public void m2() {
+
+		System.out.println("ki");
+	}
+   
+	public static void main(String[] args) {
+	
+	Git g = new Git();
+	g.m1();
+	g.m2();
+	
+}
+
+}
